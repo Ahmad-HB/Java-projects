6f4f483e09ed7c5431bc14bd9ef2568f2d1a2444
A7madcodes <142711227+A7madcodes@users.noreply.github.com>
2024-11-09 22:20:44 +0300

    Java-DS&A

---
+++ Java-DS&A/DSA2/src/IList.java
@@ -0,0 +1,13 @@
+
+public interface IList {
+	 void add(int value);
+	 int size();
+	 void print();
+	 int removeLastIndex();
+	 void resize();
+	 int get(int value);
+	 int removeAtIndex(int value);
+	
+	
+
+}
