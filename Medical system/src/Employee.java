de79915266cab9bc5c4ce66a83a666c642c0aae4
Ahmad-HB <ahmadhbahbeh2004@gmail.com>
2024-12-28 21:38:52 +0300

    Maybr-last

--- Medical system/src/Employee.java
+++ Medical system/src/Employee.java
@@ -2,7 +2,6 @@ import java.util.Random;
 
 public class Employee extends User {
 
-<<<<<<< HEAD
 	private String Role;
 	private String status;
 
@@ -12,37 +11,6 @@ public class Employee extends User {
 		this.status = Status.Free.toString();
 	}
 
-=======
-	private String Name;
-	private int ID;
-	private static int lastID=0;
-	private String Role;
-	private String status;
-
-	public Employee(String name, int id, String role) {
-
-		this.Name = name;
-		setID();
-		this.Role = role;
-		this.status = Status.Free.toString();
-	}
-
-	public String getName() {
-		return Name;
-	}
-
-	public void setName(String name) {
-		Name = name;
-	}
-
-	public int getID() {
-		return ID;
-	}
-
-	public void setID() {
-		this.ID = ++lastID;
-	}
->>>>>>> 7ed6cd0a311dae63a4f7ab86893d88abe3df7125
 
 	public String getRole() {
 		return Role;
